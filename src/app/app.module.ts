f243bd829d58a67f06c0ccf81f63fe6c8a0d7e15
Karnika Thilagar <kthilagar@AD.MSYSTECHNOLOGIES.COM>
2023-06-15 17:30:39 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SchoolComponent } from './school/school.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SchoolComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
